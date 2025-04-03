1653d603f7bfc0689285e693e9c48a4a98572f49
Hassan <2391444@cognizant.com>
2025-04-03 17:18:37 +0100

    updatedModels

--- obj/Debug/net9.0/EcommerceBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcommerceBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3a7328cd88648dfab4d30bfd742c4dd20beb6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+724351e7321f4014ac61cade1224f1c75aba9d95")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
